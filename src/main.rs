d8f203211fd91cc8e74fe4a2cd3789b77102b0bf
lydia <lydi.dah@hotmail.com>
2021-09-19 14:33:55 +0200

    cargo build rand

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::{thread_rng, Rng};
 /**
  * unwap : permet d'ignorée les erreurs
  * let
@@ -11,4 +12,14 @@ fn main(){
     println ! ("saississez votre proposition: ");
     io::stdin().read_line(&mut input).unwrap();
     println ! ("vous avez saisie {}",input);
+
+    let mut rng = thread_rng();
+    let n: u32 = rng.gen_range(0..10);
+    println!("nombre aléatoirement generé : {}", n);
 }
+/*
+ * cargo init pour lancer cargo
+ * cargo run pour compiler et executer
+ * git push pour modifié le git
+ * 
+ */
