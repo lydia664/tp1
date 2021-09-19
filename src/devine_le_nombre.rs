f4762c27aeff096309d0d0dcf8282b4141e86320
lydia <lydi.dah@hotmail.com>
2021-09-19 14:07:19 +0200

    devine le nombre

--- src/devine_le_nombre.rs
+++ src/devine_le_nombre.rs
@@ -1,4 +1,14 @@
+use std::io;
+/**
+ * unwap : permet d'ignorée les erreurs
+ * let
+ * mut
+ * readline
+ */
 fn main(){
-    println ! ("hello toust le monde \n !")
-    //let commit
+    println ! ("devine le nombre \n ");
+    let mut input = String::new();
+    println ! ("saississez votre proposition: ");
+    io::stdin().read_line(&mut input).unwrap();
+    println ! ("vous avez saisie {}",input);
 }
